b38db77ddf04a423d432ee251aa202dd36739b65
Md Mohosin Ali Shah <mohosin2126@gmail.com>
2024-08-14 16:42:03 +0600

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,19 +1,21 @@
+require("dotenv").config();
+
 module.exports = {
   "development": {
-    "username": "postgres",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "port": "5432",
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST,
+    "port": process.env.DATABASE_PORT,
     "dialect": "postgres"
   },
   "test": {
-    "username": "postgres",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "port": "5432",
-    "dialect": "postgres"
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST,
+    "port": process.env.DATABASE_PORT,
+    "dialect":"postgres"
   },
   "production": {
     "username": process.env.DATABASE_USERNAME,
